96ad994514baf77aa6e4440e18f8e823dba90848
Jonathan Chan <jchan@icebrg.us>
2012-04-12 01:16:54 -0700

    Fixed autoloader.

--- README.md
+++
@@ -1,8 +0,0 @@
-# httplang
-A small collection of functions for dealing with the HTTP Accept-Language header.
-
-Tested with PHP 5.3. May work with older versions.
-
-Tries to follow the [Semantic Versioning Specification][1].
-
-[1]: http://semver.org/
--- autoload.php
+++ autoload.php
@@ -1,11 +1,11 @@
 <?php
 /*
  * Copyright (c) 2012, Jonathan Chan <jchan@icebrg.us>
- *
+ * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -14,27 +14,22 @@
  * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-namespace jyc\httplang;
 
-require_once __DIR__ . '/autoload.php';
+/**
+ * Autoloader for loading classes in this package (by removing all but
+ * the base name).
+ */
 
-use PHPUnit_Framework_TestCase;
+spl_autoload_register(function($class) {
+    $this_package = 'jyc\\httplang';
 
-class HttpLangTest extends PHPUnit_Framework_TestCase
-{
-    public function testLanguages()
-    {
-        $languages = HttpLang::languages('da, en-gb;q=0.8, en;q=0.7');
-        $this->assertEquals(array('da'=>1, 'en-gb'=>0.8, 'en'=>0.7), $languages);
+    $file = '';
+    if (strpos($class, $this_package) === 0) {
+        $file = substr($class, strlen($this_package) + 1);
     }
+    $file .= '.php';
 
-    public function testPreferredLanguage()
-    {
-        $header = 'fr;q=0.6,de;q=0.5,en-gb;q=0.8';
-
-        $this->assertEquals('en-us',
-            HttpLang::negotiate(array('en-us', 'de', 'fr'), $header));
-        $this->assertEquals('fr',
-            HttpLang::negotiate(array('de', 'fr'), $header));
+    if (is_readable($file)) {
+        require $file;
     }
-}
+});
